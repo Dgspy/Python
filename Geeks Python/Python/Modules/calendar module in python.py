d32e1710b4717ed633de018e059263ee530f87ea
Dgspy <106894304+Dgspy@users.noreply.github.com>
2022-06-05 12:42:51 +0530

    Add files via upload

---
+++ Geeks Python/Python/Modules/calendar module in python.py
@@ -0,0 +1,8 @@
+import calendar
+yy = 2022
+mm = 1
+print(calendar.month(yy,mm))
+print("........................................................................................................................")
+import calendar
+print("the calendar of year 2005 is :-:")
+print(calendar.calendar(2005,2,1,6))
