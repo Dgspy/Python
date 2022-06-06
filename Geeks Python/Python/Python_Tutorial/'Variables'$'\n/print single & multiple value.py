65c3b124a600ae6345c79fa5a4fce2a87533388a
Dgspy <106894304+Dgspy@users.noreply.github.com>
2022-06-06 13:04:23 +0530

    Add files via upload

---
+++ Geeks Python/Python/Python_Tutorial/'Variables'$'\n/print single & multiple value.py
@@ -0,0 +1,24 @@
+# Equivalent codes in Python 3.0
+# (Produces same output)
+
+# Code 1:
+print(1)
+
+# Code 2 :
+print((1))
+print()
+
+# Code 1:
+print(1, 2)
+
+# Code 2:
+print((1, 2))
+
+# Code 3:
+# printing on the same line
+# for printing on the same line use
+# end parameters of the print function
+# end takes the values which is printing
+# at the end of the output.
+print(1, end=" ")
+print(2)
